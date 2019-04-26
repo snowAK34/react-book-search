2bab0d26c279a70f80dd1971d16e3f25b52b1b79
Stacy Nowak <nowak.stacy@gmail.com>
2019-04-25 22:18:04 -0700

    created files for components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import NavBar from './components/NavBar.js';
+import Search from './components/Search.js';
+import Results from './components/Results.js';
+import Saved from './components/Saved.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // navbar
+
+    // jumbotron
+
+    // on search page:
+      // search bar
+
+      // results
+
+    // on saved page:
+      // saved div 
   );
 }
 
